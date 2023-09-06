6aecc43b78670c411cd8511f61d9376eaeb3289f
Mateus Vitor <mateusvitor01.mv@gmail.com>
2023-09-06 00:40:22 -0300

    Post-jogador

--- src/routes/jogadorRoutes.ts
+++ src/routes/jogadorRoutes.ts
@@ -1,12 +1,11 @@
 import express from 'express';
-import { getAllJogadores, updateJogador} from '../controllers/jogadorControllers';
+import { getAllJogadores, newJogador, updateJogador} from '../controllers/jogadorControllers';
 import prisma from '../database/db';
 
 const router = express.Router();
 
 router.get('/jogadores', getAllJogadores);
-
-
 router.put('/jogador/:id', updateJogador);
+router.post('/jogador', newJogador);
 
 export default router;
