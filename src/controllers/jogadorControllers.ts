40fccc6778f78831f91bfda21571df7785c7dc4b
Ana Virginia <ana.vlc.ara@gmail.com>
2023-09-05 22:22:34 -0300

    Criando Put jogadores.

--- src/controllers/jogadorControllers.ts
+++ src/controllers/jogadorControllers.ts
@@ -19,3 +19,27 @@ export const getAllJogadores = async (req: Request, res: Response) => {
         res.status(500).json({message: `Erro interno do servidor`});
     }
 }
+
+export const updateJogador = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const {nome, idade, timeId} = req.body;    
+
+    try{
+        const updateJogador = await prisma.jogador.update({
+            where: {
+                id: id,
+            },
+            data: {
+                Nome: nome,
+                Idade: idade,
+                time_id: timeId
+            },
+        })
+
+        res.json(updateJogador);
+    }
+    catch (e) {
+        console.error(e);
+        res.status(500).json({message: 'Erro interno do servidor'});
+    }
+}
