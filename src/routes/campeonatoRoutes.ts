8c32651a6d76c94ee0554c111a6c591b052784d3
Ana Virginia <ana.vlc.ara@gmail.com>
2023-09-06 18:03:11 -0300

    Criando delete de campeonato

--- src/routes/campeonatoRoutes.ts
+++ src/routes/campeonatoRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllCampeonatos, updateCampeonato, newCampeonato, newTimeOnCampeonato} from '../controllers/campeonatoControllers';
+import { getAllCampeonatos, updateCampeonato, newCampeonato, newTimeOnCampeonato, deleteCampeonato} from '../controllers/campeonatoControllers';
 import prisma from '../database/db';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get('/campeonatos', getAllCampeonatos);
 router.put('/campeonato/:id', updateCampeonato);
 router.post('/campeonato', newCampeonato);
 router.post('/timecampeonato', newTimeOnCampeonato);
+router.delete('/campeonato/:id', deleteCampeonato);
 
 export default router;
