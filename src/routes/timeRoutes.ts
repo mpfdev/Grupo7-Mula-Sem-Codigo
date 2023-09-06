ab584ea5332af83692a5ffd7561cf8948b8aef9c
Mateus Vitor <mateusvitor01.mv@gmail.com>
2023-09-06 00:47:27 -0300

    Post-time

--- src/routes/timeRoutes.ts
+++ src/routes/timeRoutes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import { getAllTimes, updateTime} from '../controllers/timeControllers';
+import { getAllTimes, updateTime, newTime} from '../controllers/timeControllers';
 import prisma from '../database/db';
 
 const router = express.Router();
 
 router.get('/times', getAllTimes);
-
 router.put('/time/:id', updateTime);
+router.post('/time', newTime);
 
 export default router;
