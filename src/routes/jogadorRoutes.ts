eb75f27c08dbc41b9f7a26ec26f0e1adc4ad54d0
Mateus Vitor <mateusvitor01.mv@gmail.com>
2023-09-09 00:11:30 -0300

    delete-jogador

--- src/routes/jogadorRoutes.ts
+++ src/routes/jogadorRoutes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import {getAllJogadores, updateJogador, newJogador} from '../controllers/jogadorControllers';
+import {getAllJogadores, updateJogador, newJogador, deleteJogador} from '../controllers/jogadorControllers';
 
 import prisma from '../database/db';
 
@@ -9,5 +9,6 @@ const router = express.Router();
 router.get('/jogadores', getAllJogadores);
 router.put('/jogador/:id', updateJogador);
 router.post('/jogador', newJogador);
+router.delete('/jogador/:id', deleteJogador);
 
 export default router;
